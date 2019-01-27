4e251c79089a640c1831b99f0e093355abfe135f
miohsu <kunieshow@163.com>
2019-01-27 15:43:37 +0800

    no message

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
 from .views import PostListView
+from .views import PostDetailView
 
 urlpatterns = [
     # url(r'^home/$', PostListView.as_view(), name='home'),
+    url(r'^(?P<pk>[0-9]+)/$', PostDetailView.as_view(), name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic import DetailView
 
 from .models import Post, Tag, Category
 
@@ -12,4 +13,10 @@ class PostListView(ListView):
     context_object_name = 'posts'
     template_name = 'blog/home.html'
     paginate_by = 5
+
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name = 'blog/detail.html'
 
